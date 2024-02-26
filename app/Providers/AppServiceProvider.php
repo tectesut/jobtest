caf80e45e8f546fbb228f8021fa91d0307d1a39d
tectesut <tectesut@gmail.com>
2024-02-26 17:32:31 +0900

    change https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -20,6 +20,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        Paginator::useBootstrap();
+        //
+        if(\App::environment(['production'])){
+            \url::forceScheme('https');
+        }
     }
 }
